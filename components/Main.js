15ad1c5672a51fb705726e93ab6db183d9b6bfc8
artred02 <clement.lardemelle@gmail.com>
2024-01-23 18:40:49 +0100

    adding a component

---
+++ components/Main.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Main() {
+  return (
+    <View>
+      <Text>Main</Text>
+      
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
