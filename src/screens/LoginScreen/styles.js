d58ad54d2c8bad93d794c88df526284735ab45d5
ThomasDlx <thomas.delcroix1303@outlook.fr>
2024-02-08 22:07:37 +0100

    Fix logo

--- src/screens/LoginScreen/styles.js
+++ src/screens/LoginScreen/styles.js
@@ -11,7 +11,7 @@ export default StyleSheet.create({
         height: 120,
         width: 90,
         alignSelf: "center",
-        margin: 30,
+        margin:10,
         marginTop: 100
     },
     nameapp:{
