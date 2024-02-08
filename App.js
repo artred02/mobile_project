c2c38bdba478fd47a35dae577d1a0fcf57c3a33c
artred02 <clement.lardemelle@gmail.com>
2024-02-08 20:10:12 +0100

    modifying titles

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
 import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
@@ -19,16 +20,34 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         { user ? (
-          <Stack.Screen name="Home">
+          <Stack.Screen
+            name="Home"
+            options={{ 
+              headerStyle: {
+                backgroundColor: '#34495e',
+              },
+            }}>
             {props => <HomeScreen {...props} extraData={user} setUser={setUser} />}
           </Stack.Screen>
         ) : (
           <>
-            <Stack.Screen name="Login">
+            <Stack.Screen
+              name="Login"
+              options={{ 
+                headerStyle: {
+                  backgroundColor: '#34495e',
+                },
+            }}>
               {props => <LoginScreen {...props} setUser={setUser} />}
             </Stack.Screen>
-            <Stack.Screen name="Registration">
-              {props => <RegistrationScreen {...props} setUser={setUser} />}
+            <Stack.Screen
+              name="Registration"
+              options={{ 
+                headerStyle: {
+                  backgroundColor: '#34495e',
+                },
+            }}>
+              {props => <RegistrationScreen {...props} setUser={setUser}/>}
             </Stack.Screen>
           </>
         )}
@@ -36,3 +55,10 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+
+const styles = StyleSheet.create({
+  base: {
+    backgroundColor: 'black',
+  }
+})
