4f907017a538dd613092c035af029416c8af972f
artred02 <88457259+artred02@users.noreply.github.com>
2024-02-12 17:04:36 +0100

    nav

--- components/NavBottomBar.js
+++ components/NavBottomBar.js
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHome, faPlus, faUser, faMessage } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavBottomBar() {
   return (
     <View style={styles.background}>
-      
+        <View style={styles.icon}>
+          <TouchableOpacity>
+            <FontAwesomeIcon icon={faHome} size={30} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.icon}>
+          <TouchableOpacity>
+            <FontAwesomeIcon icon={faPlus} size={30} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.icon}>
+          <TouchableOpacity>
+            <FontAwesomeIcon icon={faUser} size={30} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.icon}>
+          <TouchableOpacity >
+            <FontAwesomeIcon icon={faMessage} size={30} />
+          </TouchableOpacity>
+        </View>
     </View>
   )
 }
@@ -13,5 +34,12 @@ const styles = StyleSheet.create({
     background:{
         height:80,
         backgroundColor:'#2ecc71',
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-around',
+        alignItems:'center'
+    },
+    icon:{
+
     }
 })
