7e966d037d734f5e05240ebfc7a58c5eaed227fb
artred02 <88457259+artred02@users.noreply.github.com>
2024-03-26 17:59:16 +0100

    pas grnd chose

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,9 +3,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBHr7ENFZthYaWX2LfL5SR94c2MoF1Zamg',
-    authDomain: 'mobileproject-d77de.firebaseapp.com',
-    projectId: 'mobileproject-d77de',
+    apiKey: "AIzaSyAqY2aI-3UfvDCQcQGduuDMjS1tOCWVO44",
+    authDomain: "mobileproject-d77de.firebaseapp.com",
+    databaseURL: "https://mobileproject-d77de-default-rtdb.firebaseio.com",
+    projectId: "mobileproject-d77de",
+    storageBucket: "mobileproject-d77de.appspot.com",
+    messagingSenderId: "376553348599",
+    appId: "1:376553348599:web:d7e7bec0fa1486a4d2a2f2",
+    measurementId: "G-X81XKDGMYQ"
 };
 
 if (!firebase.apps.length) {
