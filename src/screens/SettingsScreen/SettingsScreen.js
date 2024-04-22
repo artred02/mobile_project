95f63a28328a9d2a197466d1553a6eeeca3088af
ThomasDlx <thomas.delcroix1303@outlook.fr>
2024-04-22 21:10:17 +0200

    Add function to delete your account

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,22 +1,55 @@
 import React, { useEffect, useState } from 'react'
-import { Text, TouchableOpacity, View,Button,Modal, Pressable } from 'react-native'
+import { Text, TouchableOpacity, View,Button,Modal,Alert, Pressable } from 'react-native'
 import { firebase } from '../../firebase/config'
 import { collection, query, where } from "firebase/firestore";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-
-import { faUser, faGear } from '@fortawesome/free-solid-svg-icons/';
+import { faGear, faLightbulb, faTrash } from '@fortawesome/free-solid-svg-icons/';
 import NavBottomBar from '../../../components/NavBottomBar';
-
 export default function SettingsScreen(props) {
-    const [objects, setObjects] = useState([]);
 
     const confidentialite = () => {
         props.navigation.navigate('Confidentialite')
     }
 
-
+    const deleteAccountFunction = async () => {
+        Alert.alert(
+          "Confirmation",
+          "Voulez-vous vraiment supprimer votre compte ?",
+          [
+            {
+              text: "Annuler",
+              onPress: () => console.log("Suppression annulée"),
+              style: "cancel"
+            },
+            {
+              text: "Confirmer",
+              onPress: async () => {
+                const user = firebase.auth().currentUser;
+      
+                try {
+                  const firestore = firebase.firestore();
+                  await firestore.collection('users').doc(user.uid).delete();
+                  console.log("Données utilisateur supprimées de Firestore avec succès");
+                } catch (error) {
+                  console.log("Erreur lors de la suppression des données utilisateur de Firestore:", error);
+                }
+      
+                try {
+                  firebase.auth().signOut();
+                  props.setUser(null)
+                  user.delete();
+                  console.log("Compte utilisateur supprimé avec succès");
+                } catch (error) {
+                  console.log("Erreur lors de la suppression du compte utilisateur:", error);
+                }
+              }
+            }
+        ],
+        { cancelable: false }
+    );
+    };
 
     return (
         <>
@@ -34,6 +67,21 @@ export default function SettingsScreen(props) {
                         <View style={styles.information}>
                             <FontAwesomeIcon icon={faGear} style={styles.Icon} size={25}/>
                             <Text style={styles.bodyTxt}>Confidentialité</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={deleteAccountFunction}>
+                        <View style={styles.information}>
+                            <FontAwesomeIcon icon={faTrash} style={styles.Icon} size={25}/>
+                            <Text style={styles.bodyTxt}>Supprimer le compte</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.body}>
+                    <Text style={styles.title}>Thème</Text>
+                    <TouchableOpacity onPress={confidentialite}>
+                        <View style={styles.information}>
+                            <FontAwesomeIcon icon={faLightbulb} style={styles.Icon} size={25}/>
+                            <Text style={styles.bodyTxt}>Passer en Dark mode</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
